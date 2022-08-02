17db003a8316bb6de92b05622c02ecde2a0311e3
lok3ndrasingh <lokendras12@gmail.com>
2022-08-02 20:10:52 +0530

    improvements

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -14,8 +14,11 @@ export default class ContactUs extends Component {
           <div className="row">
             <aside className="eigth columns footer-widgets">
               <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
+                <h4>Email :
+                  {` `+resumeData.email}
+                </h4>
+                <h4>Phone :
+                  {` `+resumeData.phone}
                 </h4>
               </div>
             </aside>
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,122 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Lokendra Singh",
-    "role": "React Native Developer / Software Engineer",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": " I can create super fluid, Ui-rich, crash-free, cross-platform React Native applications.I have an extensive 2.5+ years of Software development experience including all phases of SDLC.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Lokendra Singh",
+  role: "React Native Developer / Software Engineer",
+  linkedinId: "https://www.linkedin.com/in/lok3ndra",
+  email : "lokendras12@gmail.com",
+  phone : "+91 9915758867",
+  skypeid: "Your skypeid",
+  roleDescription:
+    " I can create super fluid, Ui-rich, crash-free, cross-platform React Native applications.I have an extensive 2.5+ years of Software development experience including all phases of SDLC.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/lok3ndra",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://lok3ndrasingh.github.io/",
+      className: "fa fa-github",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/lokendrasinghlive",
+      className: "fa fa-instagram",
+    },
+    {
+      name: "youtube",
+      url: "https://www.instagram.com/lokendrasinghlive",
+      className: "fa fa-youtube",
+    },
+  ],
+  aboutme:
+    "I am from Chhattisgarh, a place which is known as the rice bowl of India. I am very creative. I love to code. I write lyrics, compose music, sing songs, do digital art & calligraphy. I am also passionate about traveling and meeting new people. ",
+  address: "1st Block Koramangala, Bengaluru, Karnataka, 560034",
+  website: "https://lok3ndrasingh.github.io/",
+  education: [
+    {
+      UniversityName: "Lovely Professional University, Punjab",
+      specialization: "Computer Science & Engineering",
+      MonthOfPassing: "Aug",
+      YearOfPassing: "2019",
+      Achievements: "CGPA : 8.17/10",
+    },
+    {
+      UniversityName: "JNV Korba, Chhattisgarh",
+      specialization: "12th, CBSE",
+      MonthOfPassing: "June",
+      YearOfPassing: "2013",
+      Achievements: "Percentage : 71.4%",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Codewave Technologies",
+      specialization: "Software Engineer / React Native Developer",
+      MonthOfLeaving: "July",
+      YearOfLeaving: "2022",
+      Achievements: "",
+    },
+    {
+      CompanyName: "Moblty Inc",
+      specialization: "Quality Analyst - Automation",
+      MonthOfLeaving: "Nov",
+      YearOfLeaving: "2019",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "React Native",
+    },
+    {
+      skillname: "Javascript, ES6",
+    },
+    {
+      skillname: "Data Structures & Algorithms",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Rankpedia",
+      description: "Mobile Application for students",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "AcadsHR",
+      description: "Mobile Application for HR jo search",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "Boxsoccer",
+      description: "Mobile Application for soccer players",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "Timely",
+      description: "Mobile Application for time and work management",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        "Lokendra and I worked together in Moblty Inc .He is very dedicated towards his work. He is very creative and talented. I enjoyed working with him. I wish him best of luck for his future endeavours.",
+      name: "Avneesh Patel, Business Technology Analyst - Snowflake",
+    },
+    {
+      description:
+        "Lokendra Singh is a fantastic person to work with and is not only a multi-skilled and insightful person, but also an inspiring strategist. Experienced, deadline-oriented, and intelligent person. Highly recommended.",
+      name: "Akhileshwar Maurya, Software Developer at Nineleaps",
+    },
+    {
+      description:
+        "A very hard working and dedicated employee. I interviewed him and found him well behaved, honest and intelligent. His eagerness to learn and explore is what makes him distinct. I am sure he would add value to any organization that he is a part of. ",
+      name: "Abhishek Singh, Developer at Saxo Group India",
+    },
+  ],
+};
+
+export default resumeData;
